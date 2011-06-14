aeac7fcaf4889f00be9c5ff87ad6b9bbd5e64cc5
djcp <dan@collispuro.com>
2011-06-13 21:59:56 -0400

    prepare for release.

--- category_subscriptions.php
+++ category_subscriptions.php
@@ -2,14 +2,14 @@
 /**
  * @package Category Subscriptions
  * @author Dan Collis-Puro
- * @version 0.2
+ * @version 1.0
 */
 /*
 Plugin Name: Category Subscriptions
 Plugin URI: http://www.collispuro.com
 Description: This plugin allows your registered users to subscribe to categories and receive updates.
 Author: Dan Collis-Puro
-Version: 0.2
+Version: 1.0
 Author URI: http://collispuro.com
 */
 
